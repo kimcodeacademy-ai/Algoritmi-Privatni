3261b20ffca8a089ac548da6d83b0040826a1a4d
RisteKimov <ristekimovwork@gmail.com>
2025-12-11 19:30:04 +0100

    hash tabeli obht

---
+++ src/HashTables/OBHT/Zadaci/PM10.java
@@ -0,0 +1,83 @@
+////Задача 2. Квалитет на воздух
+//// Дадени се мерења на PM10 честички за населбите во Скопjе. Ваша задача е за
+//// дадена населба да jа наjдете просечната концентрациjа на PM10 честички.
+////  Влез: Во првиот ред од влезот е даден броjот на мерења, а во секоj нареден ред се
+////  дадени населбата и концентрациjата на PM10 честички разделени со
+////  празно место. Во последниот ред е дадена населбата за коjа треба да наjдете
+////  просечна концентрациjа на PM10 честички.
+////  Излез: Просечната концентрациjа на PM10 честички за дадената населба
+////  (заокружена на 2 децимали, притоа прво нао´гате просечна концентрациjа, па заокружувате).
+////
+////        Пример:
+////        Влез:
+//8
+//Centar 319.61
+//Karposh 296.74
+//Centar 531.98
+//Karposh 316.44
+//GaziBaba 384.05
+//GaziBaba 319.3
+//Karposh 393.37
+//GaziBaba 326.42
+//Karposh
+////        Излез:
+////        335.52
+//
+//
+
+package HashTables.OBHT.Zadaci;
+
+
+import HashTables.OBHT.PodStrukturi.OBHT;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class PM10{
+    public static void main(String[] args) throws IOException {
+        int N;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        // kluc:
+        // vrednost:
+//        OBHT<String, Double> hashTable = new OBHT<>(2 * N);
+        OBHT<String, ArrayList<Double>> hashTable = new OBHT<>(2*N);
+
+        for(int i = 0; i < N; i++){
+            String input = br.readLine();
+            String [] row = input.split(" ");
+
+            String neighbourhood = row[0];
+            double pm10 = Double.parseDouble(row[1]);
+
+            if(hashTable.search(neighbourhood) == -1){
+                ArrayList<Double> list = new ArrayList<>();
+                list.add(pm10);
+                hashTable.insert(neighbourhood, list);
+            }
+            else{
+                ArrayList<Double> list = hashTable.getBucket(hashTable.search(neighbourhood)).value;
+                list.add(pm10);
+                hashTable.insert(neighbourhood, list);
+            }
+        }
+
+        String neighbourhood = br.readLine();
+        ArrayList<Double> list = hashTable.getBucket(hashTable.search(neighbourhood)).value;
+        if(list.size() > 0){
+            double sum = 0;
+            for(double v : list){
+                sum += v;
+            }
+//            System.out.println("Prosecnata vrednost e: " + sum / list.size());
+            System.out.printf("%.2f", sum / list.size());
+        }
+        else{
+            System.out.println("Nema elementi za taa opstina");
+        }
+    }
+}
+
---
+++ src/HashTables/OBHT/Zadaci/RedCross.java
@@ -0,0 +1,67 @@
+//Задача 1. Црвен крст
+//Во рамки на една хуманитарна организациjа, потребно е да се направи статистика за крвните групи кои се на располагање за донациjа, и од кои донатори.
+//Подгрупите А1+, А2+ припа´гаат на крвна група А+, додека А1- , А2- припа´гаат
+//на група А-.
+//Влез: Во првиот ред од влезот е даден броjот на парови 𝑁, а во секоj нареден
+//ред се дадени паровите (донатор, крвна група).
+//Излез: Да се испечати по колку донатори има од секоjа крвна група согласно
+//внесените податоци.
+//Пример:
+//Влез:
+//        5
+//Alek A1+
+//Dejan B−
+//Sandra A+
+//Trajce 0+
+//Rebeka A1−
+//Излез:
+//A+=2
+//B−=1
+// 0+=1
+//A−=1
+//
+
+package HashTables.OBHT.Zadaci;
+
+import HashTables.OBHT.PodStrukturi.OBHT;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class RedCross{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        // Key:
+        // Value:
+        // m/n - 0.5 -0.75
+        OBHT<String, Integer> hashtable = new OBHT<>(11);
+        for(int i = 0; i < N; i++){
+            String input = br.readLine();
+            String [] row = input.split(" ");
+            String key = row[1];
+            key = key.replaceAll("[1-2]","");
+
+            if(hashtable.search(key) == -1){
+                hashtable.insert(key, 1);
+            }
+            else{
+                int currentValue = hashtable.getBucket(hashtable.search(key)).value;
+                hashtable.insert(key, currentValue + 1);
+            }
+
+        }
+
+        System.out.println(hashtable);
+//        for(int i = 0; i < hashtable.buckets.length; i++){
+//            if(hashtable.buckets[i] != null) {
+//                System.out.println(hashtable.buckets[i].key + "=" + hashtable.buckets[i].value);
+//            }
+//        }
+    }
+}
+
+
+
